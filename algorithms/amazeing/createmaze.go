f9c42094242c1fc98e6f1c6d772520f94096933d
YourAverageMoron <ryannffc21@gmail.com>
2024-04-24 17:20:45 +0100

    Maze now backtracks to find the solution

--- algorithms/amazeing/createmaze.go
+++ algorithms/amazeing/createmaze.go
@@ -7,67 +7,79 @@ type Coordinate struct {
 }
 
 type Maze struct {
-	mazeMap    [][]rune
+	mazeMap              [][]rune
 	start, end, position Coordinate
 }
 
-
 func (maze *Maze) MoveUp() bool {
-    if(maze.position.Y == 0 || maze.mazeMap[maze.position.Y - 1][maze.position.X] == 35){
-        return false
-    }
-    maze.position.Y --
-    return true
+	if maze.position.Y == 0 || maze.mazeMap[maze.position.Y-1][maze.position.X] == 35 {
+		return false
+	}
+	maze.position.Y--
+	return true
 }
 
-
 func (maze *Maze) MoveDown() bool {
-    if(maze.position.Y == len(maze.mazeMap) - 1 || maze.mazeMap[maze.position.Y + 1][maze.position.X] == 35){
-        return false
-    }
-    maze.position.Y ++
-    return true
+	if maze.position.Y == len(maze.mazeMap)-1 || maze.mazeMap[maze.position.Y+1][maze.position.X] == 35 {
+		return false
+	}
+	maze.position.Y++
+	return true
 }
 
 func (maze *Maze) MoveRight() bool {
-    if(maze.position.X == 0  || maze.mazeMap[maze.position.Y][maze.position.X + 1] == 35){
-        return false
-    }
-    maze.position.X ++
-    return true
+	if maze.position.X == 0 || maze.mazeMap[maze.position.Y][maze.position.X+1] == 35 {
+		return false
+	}
+	maze.position.X++
+	return true
 }
 
 func (maze *Maze) MoveLeft() bool {
-    if(maze.position.X == 1 || maze.mazeMap[maze.position.Y][maze.position.X - 1] == 35){
-        return false
-    }
-    maze.position.X --
-    return true
+	if maze.position.X == 1 || maze.mazeMap[maze.position.Y][maze.position.X-1] == 35 {
+		return false
+	}
+	maze.position.X--
+	return true
 }
 
-func (maze *Maze) Display(){
-    for x :=0; x < len(maze.mazeMap); x++ {
-        for y:=0; y < len(maze.mazeMap[x]); y ++{
-            if(x == maze.position.Y && y == maze.position.X){
-                fmt.Print("X")
-                continue
-            }
-            fmt.Print(string(maze.mazeMap[x][y]))
-        }
-        fmt.Println()
-    }
+func (maze *Maze) MoveToCoordinate(c Coordinate) bool {
+	if c.Y >= len(maze.mazeMap) ||
+		c.X >= len(maze.mazeMap) ||
+		maze.mazeMap[c.Y][c.X] == 35 {
+		return false
+	}
+	maze.position = c
+	return true
+
+}
+
+func (maze *Maze) Display() {
+	for x := 0; x < len(maze.mazeMap); x++ {
+		for y := 0; y < len(maze.mazeMap[x]); y++ {
+			if x == maze.position.Y && y == maze.position.X {
+				fmt.Print("X")
+				continue
+			}
+			fmt.Print(string(maze.mazeMap[x][y]))
+		}
+		fmt.Println()
+	}
 }
 
 func CreateMaze() Maze {
-    start:= Coordinate{1, 2}
+	start := Coordinate{1, 5}
 	return Maze{
 		mazeMap: [][]rune{
 			{'#', '#', '#', '#', 'E', '#'},
+			{'#', ' ', ' ', '#', ' ', '#'},
+			{'#', ' ', '#', '#', ' ', '#'},
+			{'#', ' ', '#', '#', ' ', '#'},
 			{'#', ' ', ' ', ' ', ' ', '#'},
 			{'#', 'S', '#', '#', '#', '#'},
 		},
-		start: start,
-		end:   Coordinate{4, 0},
-        position: start,
+		start:    start,
+		end:      Coordinate{4, 0},
+		position: start,
 	}
 }
--- algorithms/amazeing/recursive.go
+++ algorithms/amazeing/recursive.go
@@ -2,27 +2,33 @@ package amazeing
 
 import "fmt"
 
+
 type MazeResult struct {
-	maze          *Maze
-	seenLocations map[Coordinate]bool
+	maze *Maze
+	path []Coordinate
 }
 
-func RecurseMaze(maze *Maze, seenLocations map[Coordinate]bool) *Maze {
-    fmt.Println(seenLocations)
+func RecurseMaze(result *MazeResult, seenLocations map[Coordinate]bool) *MazeResult {
+	maze := result.maze
 	maze.Display()
 	seenLocations[maze.position] = true
 	if maze.end.X == maze.position.X && maze.end.Y == maze.position.Y {
-		return maze
+		return result
 	}
 	if (!seenLocations[Coordinate{X: maze.position.X - 1, Y: maze.position.Y}] && maze.MoveLeft()) ||
 		(!seenLocations[Coordinate{X: maze.position.X, Y: maze.position.Y - 1}] && maze.MoveUp()) ||
 		(!seenLocations[Coordinate{X: maze.position.X + 1, Y: maze.position.Y}] && maze.MoveRight()) ||
 		(!seenLocations[Coordinate{X: maze.position.X, Y: maze.position.Y + 1}] && maze.MoveDown()) {
-		return RecurseMaze(maze, seenLocations)
+		result.path = append(result.path, maze.position)
+		return RecurseMaze(result, seenLocations)
 	}
-	panic("something went wrong")
+	var coordinate Coordinate
+	coordinate, result.path = result.path[len(result.path)-1], result.path[:len(result.path)-1]
+	result.maze.MoveToCoordinate(coordinate)
+	return RecurseMaze(result, seenLocations)
 }
 
 func SolveMaze(maze *Maze) {
-	RecurseMaze(maze, make(map[Coordinate]bool))
+    result := RecurseMaze(&MazeResult{maze: maze, path: []Coordinate{}}, make(map[Coordinate]bool))
+    fmt.Println(result.path)
 }
